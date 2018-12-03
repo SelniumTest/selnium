5d52f1f95f35fd6f33ead51f3fd678b28f191f41
SelniumTest <30858884+SelniumTest@users.noreply.github.com>
2018-12-03 17:03:16 +0800

    Update BaseTest.java

--- SeleniumService/src/test/java/com/wistron/selenium/SeleniumService/testcase/BaseTest.java
+++ SeleniumService/src/test/java/com/wistron/selenium/SeleniumService/testcase/BaseTest.java
@@ -1,6 +1,9 @@
 package com.wistron.selenium.SeleniumService.testcase;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.After;
 import org.junit.Before;
@@ -9,6 +12,9 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
